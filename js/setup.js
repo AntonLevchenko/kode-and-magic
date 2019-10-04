39bf7272e4a2da70dd3bd372e47ba5a3d0bcf480
AntonLevchenko <anton.levchenko.post@gmail.com>
2019-10-04 17:09:46 +0300

    add change color feature

--- js/setup.js
+++ js/setup.js
@@ -8,6 +8,31 @@ let similarListElement = document.querySelector('.setup-similar-list');
 let similarWizardTemplate = document.querySelector('#similar-wizard-template')
                             .content
                             .querySelector('.setup-similar-item');
+let wizardCoat = userDialog.querySelector('.wizard-coat');
+let wizardEyes = userDialog.querySelector('.wizard-eyes');
+let wizardFireball = userDialog.querySelector('.setup-fireball');
+const COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(15, 20, 55)',
+    'rgb(0, 0, 0)'
+];
+const EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+];
+const FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+];
 
 // mock
 const WIZARDS_NAME = ['Pendalf', 'Harry Potter', 'Dambldor', 'Volandemort'];
@@ -73,3 +98,29 @@ let renderWizard = function (wizard) {
 for (let i = 0; i < 4; i++) {
     similarListElement.append( renderWizard(wizards[i]) );
 }
+
+function changeColor(type) {
+    let colorIndex = 0;
+
+    return function(target, colors) {
+        colorIndex = (colorIndex < colors.length - 1) ? ++colorIndex : 0;
+        let color = colors[colorIndex];
+        target.style.fill = color;
+
+        userDialog.querySelector(`[name=${type}-color]`).value = color;
+    };
+}
+
+const changeCoatColor = changeColor('coat');
+const changeEyesColor = changeColor('eyes');
+const changeFireballColor = changeColor('fireball');
+
+wizardCoat.addEventListener('click', function (e) {
+    changeCoatColor(e.target, COAT_COLORS);
+});
+wizardEyes.addEventListener('click', function (e) {
+    changeEyesColor(e.target, EYES_COLORS);
+});
+wizardFireball.addEventListener('click', function (e) {
+    changeFireballColor(e.target, FIREBALL_COLORS);
+});
